e11420cb583c3305adc777800b56b2bb4c274bee
Manuel Avecilla <manuel.avecilla.gavilan.al@iespoligonosur.org>
2025-10-25 18:30:31 +0200

    Terminar url_1

--- app/Concursos_Online/views.py
+++ app/Concursos_Online/views.py
@@ -6,16 +6,27 @@ from .models import *
 def index(request):
     return render(request, 'Concursos_Online/index.html')
 
+# Una url que me muestre todos los Concursos y sus datos, incluido los relacionados.
 def concursos_listar(request):
-    concursos = Concurso.objects.select_related("creador").select_related("ganador").prefetch_related("participantes")
+    
+    concursos = (
+        Concurso.objects
+        .select_related(
+            "creador__usuario",  # anidado
+            "ganador__usuario",  # Asi también accedes a su usuario
+        )
+        .prefetch_related("participantes__usuario")  # Relacion N:N en participantes
+    )
     concursos.all()
+    
     """
     concursos = (Concurso.objects.raw(
     "SELECT * FROM Concursos_Online_concurso co "
-    + " JOIN Concursos_Online_administrador ad ON co.creador = ad.id "
-    + " JOIN Concursos_Online_participante pa ON co.ganador = pa.id "
+    + " JOIN Concursos_Online_administrador ad ON co.creador_id = ad.id "
+    + " JOIN Concursos_Online_participante pa ON co.ganador_id = pa.id "
     + " JOIN Concursos_Online_inscribe ins ON co.id = ins.concurso_id "
     + " JOIN Concursos_Online_participante p ON p.id = ins.participante_id"
     ))
     """
+    
     return render(request,'Concursos_Online/lista_Concurso.html',{'Concursos_Mostrar':concursos})
