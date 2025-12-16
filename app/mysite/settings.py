65667fcaf7c2efdc90d881564f4f40080eace81a
Manuel Avecilla <manuel.avecilla.gavilan.al@iespoligonosur.org>
2025-12-16 09:24:21 +0100

    Despliege

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -20,12 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ue3!j^tv0)hpzf@)^^e+2fxd)b45r_@i0v&0(=6#h&%8#&)if'
+import environ
+import os
+
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'),True)
+env = environ.Env()
+SECRET_KEY =  env("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["manuelavecilla.pythonanywhere.com"]
 
 INTERNAL_IPS = ["127.0.0.1"]
 
